15e01b25b893e2e718097f0f5d4562dc04b2a342
MargretBenjamin <margretrocks07@gmail.com>
2022-03-13 16:53:10 +0530

    New project Added

---
+++ src/test/java/TestCases/TC01.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC01 
+{
+	@Test
+	public void tc01()
+	{
+		Reporter.log("Test Case 01");
+	}
+}
